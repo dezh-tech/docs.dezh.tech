30d214bc474ab737896b9b94a4473cc48c25936d
Kay <kehiiiiya@gmail.com>
2024-12-30 18:28:17 +0000

    use docusaurus.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,7 +51,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            '',
+            'https://github.com/dezh-tech/docs.dezh.tech/edit/main/README.md',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
